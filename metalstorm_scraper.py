71fd9711c9afdefe17929c674da302d59139481b
Sergey Starchenko <syrisko@gmail.com>
2017-08-05 15:17:21 +0300

    metalstorm

--- metalstorm_scraper.py
+++ metalstorm_scraper.py
@@ -1,18 +1,18 @@
 import csv
 import logging
 import sys
+import re
 from datetime import datetime
 from selenium import webdriver
 
-
 logger = logging.getLogger("wscrapper")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
-
 driver = webdriver.PhantomJS()
+driver.maximize_window()
 curr_date = datetime.now().strftime("%Y-%m-%d")
 
 # files
@@ -20,7 +20,8 @@ band_links = "band_links_{0}.csv".format(curr_date)
 band_details = "band_details_{0}.csv".format(curr_date)
 album_links = "album_links_{0}.csv".format(curr_date)
 album_details = "album_details_{0}.csv".format(curr_date)
-
+member_links = "member_links_{0}.csv".format(curr_date)
+similar_bands = "similar_bands_{0}.csv".format(curr_date)
 
 logger.info("start")
 
@@ -36,17 +37,113 @@ def get_bands_links():
             logger.info("add band_link %s", row)
 
 
+def get_band_details():
+    with open(band_details, 'w') as f:
+        writer = csv.writer(f)
+        for band_detail_type in [
+            ("country", "country"),
+            ("bandname", "also_known_as"),
+            ("label", "labels"),
+            ("year_formed", "former_in"),
+            ("year_disbanded", "disbanded_in"),
+            ("style", "style")
+        ]:
+            for detail in driver.find_elements_by_xpath(
+                    "//table[@class='break-on-xs']//tr[./td/a[contains(@href, '{0}')]]//a".format(band_detail_type[0])):
+                row = [driver.current_url, detail.get_property("textContent").encode("utf-8"),
+                       detail.get_attribute('href'),
+                       band_detail_type[1]]
+                writer.writerow(row)
+                logger.info("add band_detail %s", row)
+
+
+def get_similar_bands():
+    with open(similar_bands, 'w') as f:
+        writer = csv.writer(f)
+        for similar in driver.find_elements_by_css_selector("#similar_bands a[href *= 'band.php']"):
+            row = [driver.current_url, similar.get_property("textContent").encode("utf-8"),
+                   similar.get_attribute('href')]
+            writer.writerow(row)
+            logger.info("add similar_bands %s", row)
+
+
+def get_band_lineup():
+    with open(member_links, 'w') as f:
+        writer = csv.writer(f)
+        for lineup_type in [
+            ("lineuptab0", "current members"),
+            ("lineuptab1", "studio musicians"),
+            ("lineuptab2", "live musicians"),
+            ("lineuptab3", "guest musicians"),
+            ("lineuptab4", "former musicians")
+        ]:
+            for album in driver.find_elements_by_css_selector(
+                    "#page-content #{0} a[href *= 'bandmember.php']".format(lineup_type[0])):
+                row = [driver.current_url, album.get_property("textContent").encode("utf-8"),
+                       album.get_attribute('href'),
+                       lineup_type[1]]
+                writer.writerow(row)
+                logger.info("add member_link %s", row)
+
+
 def get_album_links():
-    with open(band_links, 'r') as b, open(album_links, 'w') as a:
+    with open(album_links, 'w') as f:
+        writer = csv.writer(f)
+        for album_type in [
+            ("discotab1", "album"),
+            ("discotab2", "ep"),
+            ("discotab3", "other_release")
+        ]:
+            for album in driver.find_elements_by_css_selector(
+                    "#page-content #{0} a[href *= 'album.php']".format(album_type[0])):
+                row = [driver.current_url, album.get_property("textContent").encode("utf-8"),
+                       album.get_attribute('href'),
+                       album_type[1]]
+                writer.writerow(row)
+                logger.info("add album_link %s", row)
+
+
+def get_album_details():
+    with open(album_details, 'w') as f:
+        writer = csv.writer(f)
+        for album_details_type in [
+            ("//tr[contains(.,'Release date')]/td[2]", "release_date"),
+            ("//tr[contains(.,'Style')]/td[2]", "style"),
+            ("//*[contains(@class,'discography-album')]//*[contains(@class,'left-col')]//img", "cover"),
+            ("//*[contains(@class,'discography-album')]//*[contains(@id,'tracks')]", "tracks"),
+            ("//*[contains(@class,'album-rating')]//*[contains(@class,'megarating')]", "rating"),
+            ("//*[contains(@class,'album-rating')]//*[contains(@class,'votes_num')]", "votes_num")
+        ]:
+            for album in driver.find_elements_by_xpath(album_details_type[0]):
+                p = 'textContent'
+                if album_details_type[1] == 'cover':
+                    p = 'src'
+                val = album.get_property(p).encode("utf-8")
+                if album_details_type[1] == 'votes_num':
+                    val = re.search('\d+', str(val)).group()
+                row = [driver.current_url, val, album_details_type[1]]
+                writer.writerow(row)
+                logger.info("add album_details %s", row)
+
+
+def process_band_pages():
+    with open(band_links, 'r') as b:
         reader = csv.reader(b)
-        aw = csv.writer(a)
         for br in reader:
             driver.get(br[1])
-            # albums
-            for i in driver.find_elements_by_css_selector("#page-content a[href *= 'album.php']"):
-                row = [br[0], br[1], i.text.encode("utf-8"), i.get_attribute('href')]
-                aw.writerow(row)
-                logger.info("add album_link %s", row)
+            # get_bands_links()
+            get_band_details()
+            get_band_lineup()
+            get_album_links()
+            get_similar_bands()
+
+
+def process_album_pages():
+    with open(album_links, 'r') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            driver.get(line[2])
+            get_album_details()
 
 
 def init():
@@ -54,8 +151,7 @@ def init():
 
 
 init()
-# get_bands_links()
-get_album_links()
-
+process_band_pages()
+process_album_pages()
 
 driver.close()
